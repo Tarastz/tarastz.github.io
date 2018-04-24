a1cc02380a1f876b8c7c212402183bce0c0da7da
tarastz <tarastz17@gmail.com>
2018-04-25 01:04:57 +0300

    1

--- js/clock.js
+++
@@ -1,8 +0,0 @@
-var clock = document.getElementById('clock');
-setInterval(function() {
-  // Генеруємо годинник тут
-  var date = new Date();
-  var html = '';
-  html += '<h1>' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds() + '</h1>';
-  clock.innerHTML = html;
-}, 100)
